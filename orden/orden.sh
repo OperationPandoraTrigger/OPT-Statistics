884fed1995679ad6f741c9e3a671e4cccd1dee1d
formtapez <github.com@posthub.de>
2022-02-20 08:43:50 +0100

    only recent players, progressbar

--- orden/orden.sh
+++ orden/orden.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+##########################
 DB_NAME=opt
 DB_USER=opt
 DB_PASS=optpass
@@ -7,6 +8,7 @@ DB_PASS=optpass
 TEMPDIR=/tmp
 ORDENDIR=/root/log/orden
 WWWDIR=/var/www/orden
+##########################
 
 # Get most kills
 mariadb -u ${DB_USER} -p${DB_PASS} ${DB_NAME} --skip-column-names --silent --execute="SELECT PlayerUID, COUNT(KilledEnemy) AS Kills FROM Events WHERE PlayerUID != 0 GROUP BY PlayerUID ORDER BY Kills DESC LIMIT 1;" > ${TEMPDIR}/mostkills.tmp
@@ -24,10 +26,20 @@ do
 done < ${TEMPDIR}/mostdeaths.tmp
 rm ${TEMPDIR}/mostdeaths.tmp
 
-# Get all players
-mariadb -u ${DB_USER} -p${DB_PASS} ${DB_NAME} --skip-column-names --silent --execute="SELECT SteamID64, Nickname, DATE_FORMAT(Seenfirst, '%d.%m.%Y') FROM Players WHERE SteamID64 != 0;" > ${TEMPDIR}/players.tmp
+
+# Get players seen max. 9 days ago - Or get all players if called with --all
+if [[ "$*" == *"--all"* ]]
+then
+    mariadb -u ${DB_USER} -p${DB_PASS} ${DB_NAME} --skip-column-names --silent --execute="SELECT SteamID64, Nickname, DATE_FORMAT(SeenFirst, '%d.%m.%Y') AS SeenFirst FROM Players WHERE SteamID64 != 0;" > ${TEMPDIR}/players.tmp
+else
+    mariadb -u ${DB_USER} -p${DB_PASS} ${DB_NAME} --skip-column-names --silent --execute="SELECT SteamID64, Nickname, DATE_FORMAT(SeenFirst, '%d.%m.%Y') AS SeenFirst FROM Players WHERE SteamID64 != 0 AND SeenLast > SUBDATE(NOW(), 9);" > ${TEMPDIR}/players.tmp
+fi
+
+PLAYERS=`wc -l ${TEMPDIR}/players.tmp | cut -d " " -f 1`
+PLAYER=0
 while IFS=$'\t' read -r -a players
 do
+    PLAYER=$((PLAYER+1))
     STEAM64=${players[0]}
     NICK=${players[1]}
     SINCE=${players[2]}
@@ -43,78 +55,77 @@ do
 	VEHICLEKILLS=${player[8]}
 	PARTICIPATIONS=${player[20]}
 
-        echo Stats for ${NICK}: kills: $KILLS - rev: $REVIVES - flags: $FLAGS - trapo: $TRAPO - vk: $VEHICLEKILLS - participations: $PARTICIPATIONS
-
+        echo ${PLAYER}/${PLAYERS} Rendering picture for ${STEAM64} ${NICK}... \(Kills: ${KILLS} / Revives: ${REVIVES} / Flags: ${FLAGS} / TrapoKM: ${TRAPO} / VehicleKills: ${VEHICLEKILLS} / Participations: ${PARTICIPATIONS}\)
 	CMD="magick montage -background transparent "
 
 	# Dienstgrad nach Anzahl der Schlachtteilnahmen
-	if [ "$PARTICIPATIONS" -ge "50" ]; then
+	if [ "${PARTICIPATIONS}" -ge "50" ]; then
 	    CMD+="${ORDENDIR}/teamblau/general.svg "
 	else
-	    if [ "$PARTICIPATIONS" -ge "48" ]; then
+	    if [ "${PARTICIPATIONS}" -ge "48" ]; then
 		CMD+="${ORDENDIR}/teamblau/generalleutnant.svg "
 	    else
-		if [ "$PARTICIPATIONS" -ge "46" ]; then
+		if [ "${PARTICIPATIONS}" -ge "46" ]; then
 		    CMD+="${ORDENDIR}/teamblau/generalmajor.svg "
 		else
-		    if [ "$PARTICIPATIONS" -ge "44" ]; then
+		    if [ "${PARTICIPATIONS}" -ge "44" ]; then
 			CMD+="${ORDENDIR}/teamblau/brigadegeneral.svg "
 		    else
-			if [ "$PARTICIPATIONS" -ge "42" ]; then
+			if [ "${PARTICIPATIONS}" -ge "42" ]; then
 			    CMD+="${ORDENDIR}/teamblau/oberst.svg "
 			else
-			    if [ "$PARTICIPATIONS" -ge "40" ]; then
+			    if [ "${PARTICIPATIONS}" -ge "40" ]; then
 				CMD+="${ORDENDIR}/teamblau/oberstleutnant.svg "
 			    else
-				if [ "$PARTICIPATIONS" -ge "38" ]; then
+				if [ "${PARTICIPATIONS}" -ge "38" ]; then
 				    CMD+="${ORDENDIR}/teamblau/major.svg "
 				else
-				    if [ "$PARTICIPATIONS" -ge "36" ]; then
+				    if [ "${PARTICIPATIONS}" -ge "36" ]; then
 					CMD+="${ORDENDIR}/teamblau/hauptmann.svg "
 				    else
-					if [ "$PARTICIPATIONS" -ge "34" ]; then
+					if [ "${PARTICIPATIONS}" -ge "34" ]; then
 					    CMD+="${ORDENDIR}/teamblau/oberleutnant.svg "
 					else
-					    if [ "$PARTICIPATIONS" -ge "32" ]; then
+					    if [ "${PARTICIPATIONS}" -ge "32" ]; then
 						CMD+="${ORDENDIR}/teamblau/leutnant.svg "
 					    else
-						if [ "$PARTICIPATIONS" -ge "30" ]; then
+						if [ "${PARTICIPATIONS}" -ge "30" ]; then
 						    CMD+="${ORDENDIR}/teamblau/oberstabsfeldwebel.svg "
 						else
-						    if [ "$PARTICIPATIONS" -ge "28" ]; then
+						    if [ "${PARTICIPATIONS}" -ge "28" ]; then
 							CMD+="${ORDENDIR}/teamblau/stabsfeldwebel.svg "
 						    else
-							if [ "$PARTICIPATIONS" -ge ""26 ]; then
+							if [ "${PARTICIPATIONS}" -ge ""26 ]; then
 							    CMD+="${ORDENDIR}/teamblau/stabsfeldwebel.svg "
 							else
-							    if [ "$PARTICIPATIONS" -ge "24" ]; then
+							    if [ "${PARTICIPATIONS}" -ge "24" ]; then
 								CMD+="${ORDENDIR}/teamblau/hauptfeldwebel.svg "
 							    else
-								if [ "$PARTICIPATIONS" -ge "22" ]; then
+								if [ "${PARTICIPATIONS}" -ge "22" ]; then
 								    CMD+="${ORDENDIR}/teamblau/oberfeldwebel.svg "
 								else
-								    if [ "$PARTICIPATIONS" -ge "20" ]; then
+								    if [ "${PARTICIPATIONS}" -ge "20" ]; then
 									CMD+="${ORDENDIR}/teamblau/feldwebel.svg "
 								    else
-									if [ "$PARTICIPATIONS" -ge "18" ]; then
+									if [ "${PARTICIPATIONS}" -ge "18" ]; then
 									    CMD+="${ORDENDIR}/teamblau/stabsunteroffizier.svg "
 									else
-									    if [ "$PARTICIPATIONS" -ge "16" ]; then
+									    if [ "${PARTICIPATIONS}" -ge "16" ]; then
 										CMD+="${ORDENDIR}/teamblau/unteroffizier.svg "
 									    else
-										if [ "$PARTICIPATIONS" -ge "14" ]; then
+										if [ "${PARTICIPATIONS}" -ge "14" ]; then
 										    CMD+="${ORDENDIR}/teamblau/oberstabsgefreiter.svg "
 										else
-										    if [ "$PARTICIPATIONS" -ge "12" ]; then
+										    if [ "${PARTICIPATIONS}" -ge "10" ]; then
 											CMD+="${ORDENDIR}/teamblau/stabsgefreiter.svg "
 										    else
-											if [ "$PARTICIPATIONS" -ge "10" ]; then
+											if [ "${PARTICIPATIONS}" -ge "6" ]; then
 											    CMD+="${ORDENDIR}/teamblau/hauptgefreiter.svg "
 											else
-											    if [ "$PARTICIPATIONS" -ge "8" ]; then
+											    if [ "${PARTICIPATIONS}" -ge "4" ]; then
 												CMD+="${ORDENDIR}/teamblau/obergefreiter.svg "
 											    else
-												if [ "$PARTICIPATIONS" -ge "6" ]; then
+												if [ "${PARTICIPATIONS}" -ge "2" ]; then
 												    CMD+="${ORDENDIR}/teamblau/gefreiter.svg "
 												else
 												    CMD+="${ORDENDIR}/teamblau/schuetze.svg "
@@ -143,83 +154,83 @@ do
 	fi
 
 	# Kills
-	if [ "$KILLS" -ge "150" ]; then
+	if [ "${KILLS}" -ge "150" ]; then
 	    CMD+="${ORDENDIR}/orden/kills_gold.svg "
 	else
-	    if [ "$KILLS" -ge "100" ]; then
+	    if [ "${KILLS}" -ge "100" ]; then
 		CMD+="${ORDENDIR}/orden/kills_silber.svg "
 	    else
-		if [ "$KILLS" -ge "50" ]; then
+		if [ "${KILLS}" -ge "50" ]; then
 		    CMD+="${ORDENDIR}/orden/kills_bronze.svg "
 		fi
 	    fi
 	fi
 
 	# Wiederbelebungen
-	if [ "$REVIVES" -ge "100" ]; then
+	if [ "${REVIVES}" -ge "100" ]; then
 	    CMD+="${ORDENDIR}/orden/sani_gold.svg "
 	else
-	    if [ "$REVIVES" -ge "50" ]; then
+	    if [ "${REVIVES}" -ge "50" ]; then
 		CMD+="${ORDENDIR}/orden/sani_silber.svg "
 	    else
-		if [ "$REVIVES" -ge "25" ]; then
+		if [ "${REVIVES}" -ge "25" ]; then
 		    CMD+="${ORDENDIR}/orden/sani_bronze.svg "
 		fi
 	    fi
 	fi
 
 	# Flaggeneroberungen
-	if [ "$FLAGS" -ge "25" ]; then
+	if [ "${FLAGS}" -ge "25" ]; then
 	    CMD+="${ORDENDIR}/orden/fahne_gold.svg "
 	else
-	    if [ "$FLAGS" -ge "10" ]; then
+	    if [ "${FLAGS}" -ge "10" ]; then
 		CMD+="${ORDENDIR}/orden/fahne_silber.svg "
 	    else
-		if [ "$FLAGS" -ge "5" ]; then
+		if [ "${FLAGS}" -ge "5" ]; then
 		    CMD+="${ORDENDIR}/orden/fahne_bronze.svg "
 		fi
 	    fi
 	fi
 
 	# Transport-Kilometer (Pilot + Fahrer + Kapitän)
-	if [ "$TRAPO" -ge "1000" ]; then
+	if [ "${TRAPO}" -ge "1000" ]; then
 	    CMD+="${ORDENDIR}/orden/trapo_gold.svg "
 	else
-	    if [ "$TRAPO" -ge "500" ]; then
+	    if [ "${TRAPO}" -ge "500" ]; then
 		CMD+="${ORDENDIR}/orden/trapo_silber.svg "
 	    else
-		if [ "$TRAPO" -ge "200" ]; then
+		if [ "${TRAPO}" -ge "200" ]; then
 		    CMD+="${ORDENDIR}/orden/trapo_bronze.svg "
 		fi
 	    fi
 	fi
 
 	# Fahrzeugzerstörungen
-	if [ "$VEHICLEKILLS" -ge "50" ]; then
+	if [ "${VEHICLEKILLS}" -ge "50" ]; then
 	    CMD+="${ORDENDIR}/orden/pa_gold.svg "
 	else
-	    if [ "$VEHICLEKILLS" -ge "25" ]; then
+	    if [ "${VEHICLEKILLS}" -ge "25" ]; then
 		CMD+="${ORDENDIR}/orden/pa_silber.svg "
 	    else
-		if [ "$VEHICLEKILLS" -ge "10" ]; then
+		if [ "${VEHICLEKILLS}" -ge "10" ]; then
 		    CMD+="${ORDENDIR}/orden/pa_bronze.svg "
 		fi
 	    fi
 	fi
 
 	# Purple Heart für die meisten Kills
-	if [ "$STEAM64" == "$MOSTKILLS" ]; then
+	if [ "${STEAM64}" == "${MOSTKILLS}" ]; then
 	    CMD+="${ORDENDIR}/orden/purple_heart.svg "
 	fi
 
 	# Purple Heart für die meisten Tode
-	if [ "$STEAM64" == "$MOSTDEATHS" ]; then
+	if [ "${STEAM64}" == "${MOSTDEATHS}" ]; then
 	    CMD+="${ORDENDIR}/orden/purple_heart.svg "
 	fi
 
 	# Bild rendern
-	CMD+="-tile x1 -geometry +5 -gravity north ${TEMPDIR}//tmp.png"
-	$CMD
+	CMD+="-tile x1 -geometry +5 -gravity north ${TEMPDIR}/tmp.png"
+	${CMD}
 
 	# Text ins Bild schreiben
 	magick convert ${TEMPDIR}/tmp.png -resize x600 -fill white -pointsize 32 -draw "text 300,515 '${NICK}'" -pointsize 16 -draw "text 300,550 'Bei der OPT seit dem ${SINCE}'" ${WWWDIR}/${STEAM64}.png
